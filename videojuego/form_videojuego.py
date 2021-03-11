df915a124eeda61cb02b45c798ed49dd6b66a14c
juca <garciamjuancarlos14@gmail.com>
2021-03-11 11:28:21 -0600

    Se eliminaron los errores con los iconos del dashboard, se agregó un ícono a la página y se modificó el widget de categoría para videojuego

--- videojuego/form_videojuego.py
+++ videojuego/form_videojuego.py
@@ -14,7 +14,7 @@ class VideoJuegoForm(forms.ModelForm):
         widgets = {
             'titulo' : forms.TextInput(attrs={'class' : 'form-control','placeholder' : 'Nombre del videojuego','onFocus' : 'validar(this)'}),
             'anio' : forms.NumberInput(attrs={'class' : 'form-control','placeholder' : 'Año de lanzamiento'}),
-            'categoria' : forms.NumberInput(attrs={'class' : 'form-control','placeholder' : 'Categoría'}),
+            'categoria' : forms.Select(attrs={'class' : 'form-control','placeholder' : 'Categoría'}),
             'precio' : forms.NumberInput(attrs={'class' : 'form-control','placeholder' : 'Precio US'}),
-            'descripcion' : forms.Textarea(attrs={'class' : 'form-control','placeholder' : 'Descripción'})
+            'descripcion' : forms.Textarea(attrs={'class' : 'form-control','placeholder' : 'Descripción','rows' : 60})
         }
